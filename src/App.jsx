66a515236516a99a11e4d3d1c90e2ab31f7dc77a
NotePCHikin <rgon1192@gmail.com>
2023-12-26 20:24:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,305 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="text-gray-500 border-b border-gray-200">
+        <div className="flex container mx-auto p-5 flex-col md:flex-row items-center">
+          <a href="#" className="font-medium text-gray-900 mb-4 md:md-0">
+            <span className="text-xl ml-3">ShinCode</span>
+          </a>
+          <nav className="md:ml-auto text-base">
+            <a href="#home" className="mr-5 hover:text-blue-400 duration-300">
+              Home
+            </a>
+            <a href="#about" className="mr-5 hover:text-blue-400 duration-300">
+              about
+            </a>
+            <a href="#slills" className="mr-5 hover:text-blue-400 duration-300">
+              Skills
+            </a>
+            <a href="#blog" className="hover:text-blue-400 duration-300">
+              Blog
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      <section className="text-gray-700" id="home">
+        <div className="flex container mx-auto py-20 px-5 flex-col md:flex-row items-center ">
+          {/* left */}
+          <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
+            <h1 className="text-3xl sm:text-6xl text-gray-900 font-medium mb-4">
+              Hi!
+              <br />
+              I'm ShinCode
+              <br />
+              Wed Developer
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+              harum dolor, consectetur praesentium iste sed quisquam explicabo
+              dolorem voluptatem tempore quia repudiandae, tempora fugit ullam,
+              maxime impedit necessitatibus numquam. Eius.
+            </p>
+            <button className="text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300">
+              Contact
+            </button>
+          </div>
+          {/* right */}
+          <div className="md:w-1/2 lg:max-w-lg w-5/6">
+            <img src="img/icon.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-700 border-t border-gray-200">
+        <div className="container px-5 py-24 mx-auto">
+          {/* about Me */}
+          <div className="text-center mb-20">
+            <h1 className="text-2xl sm:text-3xl font-medium mb-2 text-gray-900">
+              about Me
+            </h1>
+            <p className="pb-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus consectetur atque laudantium nulla maxime ipsam odit
+              nam quae velit unde.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+              deserunt tempora exercitationem molestias. Laboriosam cumque quod,
+              a ex minima quisquam sapiente eum culpa molestias aliquam facere
+              magnam officiis consequatur aspernatur iure nobis recusandae non
+              eaque, consectetur quae veniam, dolorum id quidem possimus.
+              Doloribus sapiente ipsum officia rem vero soluta natus.
+              Necessitatibus iure voluptas amet neque quaerat excepturi pariatur
+              exercitationem quidem deleniti possimus minima quae, dolore
+              laborum quibusdam dolorem ipsam consequatur quos accusantium
+              laudantium? Ducimus mollitia neque vitae quidem, quo fuga
+              expedita. Accusamus, repudiandae, totam nisi porro accusantium
+              molestias incidunt dolorum molestiae obcaecati delectus nulla
+              autem, tenetur inventore id recusandae sunt?
+            </p>
+          </div>
+          {/* ｶｰﾄﾞのdivタグ */}
+          <div className="flex flex-wrap">
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-200 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    Web Developer
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                    magni deserunt iusto! Accusamus tempore ab tenetur dolores
+                    officiis earum labore?
+                  </p>
+                  <a href="#" className="flex mt-3 text-green-500 items-center">
+                    もっと見る
+                    <svg
+                      className="ml-1"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12.0519 14.8285L13.4661 16.2427L17.7087 12L13.4661 7.7574L12.0519 9.17161L13.8803 11H6.34318V13H13.8803L12.0519 14.8285Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M1 19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5C2.79086 1 1 2.79086 1 5V19ZM5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-200 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    Web Developer
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                    magni deserunt iusto! Accusamus tempore ab tenetur dolores
+                    officiis earum labore?
+                  </p>
+                  <a href="#" className="flex mt-3 text-green-500 items-center">
+                    もっと見る
+                    <svg
+                      className="ml-1"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12.0519 14.8285L13.4661 16.2427L17.7087 12L13.4661 7.7574L12.0519 9.17161L13.8803 11H6.34318V13H13.8803L12.0519 14.8285Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M1 19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5C2.79086 1 1 2.79086 1 5V19ZM5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-200 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className="text-gray-900 text-lg font-medium ml-2">
+                    Web Developer
+                  </h2>
+                </div>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                    magni deserunt iusto! Accusamus tempore ab tenetur dolores
+                    officiis earum labore?
+                  </p>
+                  <a href="#" className="flex mt-3 text-green-500 items-center">
+                    もっと見る
+                    <svg
+                      className="ml-1"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12.0519 14.8285L13.4661 16.2427L17.7087 12L13.4661 7.7574L12.0519 9.17161L13.8803 11H6.34318V13H13.8803L12.0519 14.8285Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M1 19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5C2.79086 1 1 2.79086 1 5V19ZM5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-700 border-t border-gray-200">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          {/* left side */}
+          <div className="lg:w-1/2 mb-10 lg:mb-0 flex justify-center">
+            <img src="./img/pc.jpg" alt="" className="rounded-lg" />
+          </div>
+          {/* right side */}
+          <div className="lg:pl-12 lg:pt-6 w-full lg:w-1/2 ">
+            <h1 className="text-2xl sm:text-3xl font-medium text-gray-900 mb-10 text-center lg:text-left">
+              My Skill
+            </h1>
+            <div>
+              <h2>HTML</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="bg-green-600 text-xs leading-none text-center text-white"
+                  style={{ width: "85%" }}
+                >
+                  85%
+                </div>
+              </div>
+              <h2>CSS</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="bg-green-600 text-xs leading-none text-center text-white"
+                  style={{ width: "80%" }}
+                >
+                  80%
+                </div>
+              </div>
+              <h2>Javascript</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="bg-green-600 text-xs leading-none text-center text-white"
+                  style={{ width: "65%" }}
+                >
+                  65%
+                </div>
+              </div>
+              <h2>Tailwindcss</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="bg-green-600 text-xs leading-none text-center text-white"
+                  style={{ width: "75%" }}
+                >
+                  75%
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
